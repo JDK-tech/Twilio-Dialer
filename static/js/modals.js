5ec76d52f1299c468f337024491ea8f61fa4676c
John <jdk_official1@outlook.com>
2025-06-10 19:25:32 +0100

    Fixed incoming call modal triggering

--- static/js/modals.js
+++ static/js/modals.js
@@ -1,101 +1,76 @@
 $(document).ready(function () {
-    //////////////          DIAL MODAL         ///////////////////////////////
-
-
-    let btnOpenNumberPad = document.getElementById('btnOpenNumberPad')
-    let inputPhoneNumber = document.getElementById('phoneNumber')
-    let btnDelete = document.getElementById('btnDelete')
-
+    let btnOpenNumberPad = document.getElementById('btnOpenNumberPad');
+    let inputPhoneNumber = document.getElementById('phoneNumber');
+    let btnDelete = document.getElementById('btnDelete');
 
     btnOpenNumberPad.addEventListener('click', (event) => {
-        $('#modal-dial').modal('show')
-    })
-
+        $('#modal-dial').modal('show');
+    });
 
     $('#btnCloseDialModal').bind('click', function () {
-        $('#modal-dial').modal('hide')
+        $('#modal-dial').modal('hide');
     });
 
     $('.btnNumber').bind('click', function () {
-        let text = $(this).text()
-        inputPhoneNumber.value += text
+        let text = $(this).text();
+        inputPhoneNumber.value += text;
     });
 
-
     btnDelete.addEventListener('click', (event) => {
-        console.log('clicked', event)
-        var str = inputPhoneNumber.value
+        console.log('clicked', event);
+        var str = inputPhoneNumber.value;
         var position = inputPhoneNumber.selectionStart - 1;
 
         str = str.substr(0, position) + '' + str.substr(position + 1);
-        inputPhoneNumber.value = str
-    })
-
-
-    ///////////////       CALL IN PROGRESS MODAL          ////////////////////////////
-
-
-    $("#modal-call-in-progress").on('shown.bs.modal', function () {
-        showCallDuration()
+        inputPhoneNumber.value = str;
     });
 
+    // Ensure Incoming Calls Trigger the Modal
+    device.on("incoming", function (conn) {
+        log("Incoming call from: " + conn.parameters.From);
+        $("#callerNumber").text(conn.parameters.From);
+        $("#txtPhoneNumber").text(conn.parameters.From);
+        $('#modal-incomming-call').modal('show');
+
+        $('.btnReject').unbind().bind('click', function () {
+            $('.modal').modal('hide');
+            log("Rejected call.");
+            conn.reject();
+        });
+
+        $('.btnAcceptCall').unbind().bind('click', function () {
+            $('.modal').modal('hide');
+            log("Accepted call...");
+            conn.accept();
+        });
+    });
+
+    // Call Duration Timer
+    $("#modal-call-in-progress").on('shown.bs.modal', function () {
+        showCallDuration();
+    });
 
     function showCallDuration() {
         let output = document.getElementById('callDuration');
-        let ms = 0;
-        let sec = 0;
-        let min = 0;
+        let ms = 0, sec = 0, min = 0;
 
         function timer() {
             ms++;
-            if (ms >= 100) {
-                sec++
-                ms = 0
-            }
-            if (sec === 60) {
-                min++
-                sec = 0
-            }
-            if (min === 60) {
-                ms,
-                    sec,
-                    min = 0;
-            }
+            if (ms >= 100) { sec++; ms = 0; }
+            if (sec === 60) { min++; sec = 0; }
 
-            let milli = ms < 10 ? `0` + ms : ms;
-            let seconds = sec < 10 ? `0` + sec : sec;
-            let minute = min < 10 ? `0` + min : min;
-
-            let timer = `${minute}:${seconds}`;
+            let timer = `${min < 10 ? '0' + min : min}:${sec < 10 ? '0' + sec : sec}`;
             output.innerHTML = timer;
         };
 
-        //Start timer
-        function start() {
-            time = setInterval(timer, 10);
-        }
+        function start() { time = setInterval(timer, 10); }
+        function stop() { clearInterval(time); }
+        function reset() { ms = sec = min = 0; output.innerHTML = "00:00"; }
 
-        //stop timer
-        function stop() {
-            clearInterval(time)
-        }
-        
-        //reset timer
-        function reset() {
-            ms = 0;
-            sec = 0;
-            min = 0;
-
-            output.innerHTML = `00:00:00`
-        }
-
-        // start the timer
-        start()
+        start();
 
         $("#modal-call-in-progress").on('hidden.bs.modal', function () {
-            stop()
+            stop();
         });
-
     }
-
 });
